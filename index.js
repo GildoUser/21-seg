bf6040e0519a793e5301e0821bf6ec333754f17b
GildoUser <gildouser@gmail.com>
2023-11-24 01:23:30 -0500

    Adicionado tetreiro, com certo bug na rotação, mas ok

--- index.js
+++ index.js
@@ -1,70 +1,73 @@
 const items = document.querySelectorAll(".texts-presentation-letter");
 
-function smooth(){
-
-}
-function notSmooth(){
-    
-}
 function sleep(ms){
     return new Promise(resolve=>setTimeout(resolve,1500))
 }
 const maxitens = items.length;
 
-let currentItem = 0;
+let currentItem = 1;
+items[0].scrollIntoView({
+    inline:"start",
+    behavior:"auto"
+    
+})
 let flag = false;
-setInterval(() => {
+const teste = sleep().then(()=>{
+    setInterval(() => {
+        
     
-    
-    items.forEach((item)=>{
-        item.classList.remove("current-item")
-    })
-//se for iugal a 4
-    if(currentItem == maxitens){
-        flag = true;
-        console.log('entrou',currentItem)
-        currentItem = 2
-
-        items[currentItem].scrollIntoView({
-            inline:"start",
-            behavior:"smooth"
-            
+        items.forEach((item)=>{
+            item.classList.remove("current-item")
         })
-
-
-    }
-// se for == 3
-   // else if (currentItem == maxitens-1) {
-    //    console.log('to aqui hein',currentItem)
-     //   items[currentItem].scrollIntoView({
-    //        inline:"start",
-     //       behavior:"smooth"
-     //   })
+    //se for iugal a 4
+        if(currentItem == maxitens){
+            flag = true;
+            console.log('entrou',currentItem)
+            currentItem = 1
     
-
-   // }
-
-    else{
-        console.log('indo',currentItem)
-        items[currentItem].scrollIntoView({
-            inline:"start",
-            behavior:"smooth"
-        })
-    }
-
+            items[currentItem].scrollIntoView({
+                inline:"start",
+                behavior:"smooth"
+                
+            })
     
-
-    console.log(currentItem)
-    items[currentItem].classList.add("current-item");
-    if(flag){
-        currentItem = 0;
-        items[currentItem].scrollIntoView({
-            inline:"start",
-            behavior:"auto"
-            
-        })
-        flag = false
-    }
-    currentItem ++
     
-}, 1550);
+        }
+    // se for == 3
+       // else if (currentItem == maxitens-1) {
+        //    console.log('to aqui hein',currentItem)
+         //   items[currentItem].scrollIntoView({
+        //        inline:"start",
+         //       behavior:"smooth"
+         //   })
+        
+    
+       // }
+    
+        else{
+            console.log('mudando para',currentItem)
+            items[currentItem].scrollIntoView({
+                inline:"nearest",
+                behavior:"smooth"
+            })
+        }
+    
+        
+    
+        console.log(currentItem)
+        items[currentItem].classList.add("current-item");
+        if(flag){
+            currentItem = 0;
+            items[currentItem].scrollIntoView({
+                inline:"start",
+                behavior:"auto"
+                
+            })
+            flag = false
+        }
+        currentItem ++
+        
+    }, 4500);
+
+})
+    
